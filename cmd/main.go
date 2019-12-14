5ac2d0e551f9618c4a56ce7f80caa70a41a41ab3
Abhilash Gnan <abhilash.gnan@gmail.com>
2019-12-15 00:15:18 +0530

    init repo

---
+++ cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+
+	"github.com/jan25/gosample/custom"
+	"github.com/jan25/gosample/internal"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) < 3 {
+		fmt.Println(`usage:
+		$ gosample [m|a|p] 3 4`)
+		return
+	}
+
+	a := int(args[1])
+	b := int(args[2])
+	switch args[0] {
+	case 'm':
+		fmt.Println("%d*%d=%d", a, b, mathlib.Mult(a, b))
+	case 'a':
+		fmt.Println("%d+%d=%d", a, b, mathlib.Add(a, b))
+	case 'p':
+		fmt.Println("%d^%d=%d", a, b, mymath.Pow(a, b))
+	default:
+		fmt.Println("ERROR: invalid args")
+	}
+}
